c0511c113d918e871f6e492fbe3c1e3a8cc018c6
Mior Muhammad Zaki <crynobone@gmail.com>
2017-10-03 19:58:24 +0800

    Add additional test.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/RinggitTest.php
+++ tests/RinggitTest.php
@@ -24,4 +24,30 @@ class RinggitTest extends TestCase
         $this->assertSame('30', $money->getVatAmount());
         $this->assertSame('530', $money->getAmountWithVat());
     }
+
+    /** @test */
+    public function it_can_be_allocated_with_vat()
+    {
+        $money = new MYR(500);
+        $money->withVat();
+
+        $allocation = $money->allocateWithVat([1, 1, 1]);
+
+        $this->assertSame('177', $allocation[0]->getAmount());
+        $this->assertSame('177', $allocation[1]->getAmount());
+        $this->assertSame('176', $allocation[2]->getAmount());
+    }
+
+    /** @test */
+    public function it_can_be_allocated_with_vat_using_n()
+    {
+        $money = new MYR(500);
+        $money->withVat();
+
+        $allocation = $money->allocateWithVatTo(3);
+
+        $this->assertSame('177', $allocation[0]->getAmount());
+        $this->assertSame('177', $allocation[1]->getAmount());
+        $this->assertSame('176', $allocation[2]->getAmount());
+    }
 }
