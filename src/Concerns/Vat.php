fab7e352d5f0fc7955591ac329f98ef1d3946631
Mior Muhammad Zaki <crynobone@gmail.com>
2017-10-03 20:30:31 +0800

    Abstract concern.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ src/Concerns/Vat.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace Duit\Concerns;
+
+trait Vat
+{
+    /**
+     * Enable GST/VAL calculation.
+     *
+     * @var bool
+     */
+    protected $vat = false;
+
+    /**
+     * Make object with GST/VAT.
+     *
+     * @param int|string $amount
+     * @return static
+     */
+    public static function afterVat($amount)
+    {
+        return static::beforeVat(
+            static::asMoney($amount)->divide(1.06)->getAmount()
+        );
+    }
+
+    /**
+     * Make object without GST/VAT.
+     *
+     * @param int|string $amount
+     * @return static
+     */
+    public static function withoutVat($amount)
+    {
+        return (new static($amount))->disableVat();
+    }
+
+    /**
+     * Make object before applying GST/VAT.
+     *
+     * @param int|string $amount
+     * @return static
+     * @param  [type] $amount [description]
+     * @return [type]         [description]
+     */
+    public static function beforeVat($amount)
+    {
+        return (new static($amount))->enableVat();
+    }
+
+    /**
+     * Enable GST/VAT for calculation.
+     *
+     * @return $this
+     */
+    public function enableVat()
+    {
+        $this->vat = true;
+
+        return $this;
+    }
+
+    /**
+     * Disable GST/VAT for calculation.
+     *
+     * @return $this
+     */
+    public function disableVat()
+    {
+        $this->vat = false;
+
+        return $this;
+    }
+
+    /**
+     * Get GST/VAT amount.
+     *
+     * @return int
+     */
+    public function getVatAmount()
+    {
+        if (! $this->vat) {
+            return '0';
+        }
+
+        return $this->money->multiply(0.06)->getAmount();
+    }
+
+    /**
+     * Returns the value represented by this object with GST/VAT.
+     *
+     * @return string
+     */
+    public function getAmountWithVat()
+    {
+        if (! $this->vat) {
+            return $this->money->getAmount();
+        }
+
+        return $this->money->multiply(1.06)->getAmount();
+    }
+
+    /**
+     * Allocate the money according to a list of ratios with GST/VAT.
+     *
+     * @param  array  $ratios
+     * @return Money[]
+     */
+    public function allocateWithVat(array $ratios)
+    {
+        $results = [];
+        $allocates = static::asMoney($this->getAmountWithVat())->allocate($ratios);
+
+        foreach ($allocates as $allocate) {
+            $results[] = static::afterVat($allocate->getAmount());
+        }
+
+        return $results;
+    }
+
+    /**
+     * Allocate the money among N targets with GST/VAT.
+     *
+     * @param  int  $n
+     * @return Money[]
+     *
+     * @throws \InvalidArgumentException If number of targets is not an integer
+     */
+    public function allocateWithVatTo($n)
+    {
+        $results = [];
+        $allocates = static::asMoney($this->getAmountWithVat())->allocateTo($n);
+
+        foreach ($allocates as $allocate) {
+            $results[] = static::afterVat($allocate->getAmount());
+        }
+
+        return $results;
+    }
+
+    /**
+     * Build money object
+     *
+     * @param  int|string  $amount
+     * @return \Money\Money
+     */
+    abstract protected static function asMoney($amount);
+}
--- src/MYR.php
+++ src/MYR.php
@@ -8,6 +8,8 @@ use BadMethodCallException;
 
 class MYR
 {
+    use Concerns\Vat;
+
     /**
      * Money implementation.
      *
@@ -16,153 +18,34 @@ class MYR
     protected $money;
 
     /**
-     * Enable GST/VAL calculation.
-     *
-     * @var bool
-     */
-    protected $vat = false;
-
-    /**
      * Construct a new MYR money.
      *
      * @param int|string $amount
      */
     public function __construct($amount)
     {
-        $this->money = new Money($amount, new Currency('MYR'));
+        $this->money = static::asMoney($amount);
     }
 
     /**
-     * Make object with GST/VAT.
+     * Build money object
      *
-     * @param int|string $amount
-     * @return static
+     * @param  int|string  $amount
+     * @return \Money\Money
      */
-    public static function afterVat($amount)
+    protected static function asMoney($amount)
     {
-        $money = new Money($amount, new Currency('MYR'));
-
-        return (new static($money->divide(1.06)->getAmount()))->enableVat();
+        return new Money($amount, new Currency('MYR'));
     }
 
     /**
-     * Make object without GST/VAT.
+     * Passthrough method call to Money\Money.
      *
-     * @param int|string $amount
-     * @return static
+     * @param  string  $method
+     * @param  array  $parameters
+     * @return mixed
+     * @throws \BadMethodException If method doesn't exist.
      */
-    public static function withoutVat($amount)
-    {
-        return new static($amount);
-    }
-
-    /**
-     * Make object before applying GST/VAT.
-     *
-     * @param int|string $amount
-     * @return static
-     * @param  [type] $amount [description]
-     * @return [type]         [description]
-     */
-    public static function beforeVat($amount)
-    {
-        $money = new Money($amount, new Currency('MYR'));
-
-        return (new static($amount))->enableVat();
-    }
-
-    /**
-     * Enable GST/VAT for calculation.
-     *
-     * @return $this
-     */
-    public function enableVat()
-    {
-        $this->vat = true;
-
-        return $this;
-    }
-
-    /**
-     * Disable GST/VAT for calculation.
-     *
-     * @return $this
-     */
-    public function disableVat()
-    {
-        $this->vat = false;
-
-        return $this;
-    }
-
-    /**
-     * Get GST/VAT amount.
-     *
-     * @return int
-     */
-    public function getVatAmount()
-    {
-        if (! $this->vat) {
-            return '0';
-        }
-
-        return $this->money->multiply(0.06)->getAmount();
-    }
-
-    /**
-     * Returns the value represented by this object with GST/VAT.
-     *
-     * @return string
-     */
-    public function getAmountWithVat()
-    {
-        if (! $this->vat) {
-            return $this->money->getAmount();
-        }
-
-        return $this->money->multiply(1.06)->getAmount();
-    }
-
-    /**
-     * Allocate the money according to a list of ratios with GST/VAT.
-     *
-     * @param  array  $ratios
-     * @return Money[]
-     */
-    public function allocateWithVat(array $ratios)
-    {
-        $results = [];
-        $allocates = (new Money($this->getAmountWithVat(), new Currency('MYR')))
-                            ->allocate($ratios);
-
-        foreach ($allocates as $allocate) {
-            $results[] = static::afterVat($allocate->getAmount());
-        }
-
-        return $results;
-    }
-
-    /**
-     * Allocate the money among N targets with GST/VAT.
-     *
-     * @param  int  $n
-     * @return Money[]
-     *
-     * @throws \InvalidArgumentException If number of targets is not an integer
-     */
-    public function allocateWithVatTo($n)
-    {
-        $results = [];
-        $allocates = (new Money($this->getAmountWithVat(), new Currency('MYR')))
-                            ->allocateTo($n);
-
-        foreach ($allocates as $allocate) {
-            $results[] = static::afterVat($allocate->getAmount());
-        }
-
-        return $results;
-    }
-
     public function __call($method, array $parameters)
     {
         if (! method_exists($this->money, $method)) {
